37c68e04d29d1aadc31f42358f3e03207872b811
Majd Alhourani <majd.aldein.alhournai@gmaail.com>
2025-12-10 15:46:26 -0800

    create our policy section

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -3,19 +3,20 @@ import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
 import ProductItem from './ProductItem';
 
-const LatestCollecton = () => {
+const BestSeller = () => {
   const { products } = useContext(ShopContext);
-  const [latestProducts, setLatestProducts] = useState([]);
+  const [bestSeller, setBestSeller] = useState([]);
 
   useEffect(() => {
-    setLatestProducts(products.slice(0, 10));
+    const bestSellerProducts = products.filter((item) => item.bestseller);
+    setBestSeller(bestSellerProducts.slice(0, 5));
   }, []);
 
   return (
     <div className="my-10">
-      <div className="text-center py-9 text-3xl">
-        <Title text1="LATEST" text2="COLLECTIONS" />
-        <p className="w-3/4 m-auto text-xs sm:text-base text-gray-700">
+      <div className="text-center text-3xl py-8">
+        <Title text1="BEST" text2="SELLERS" />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia hic, dolorem autem modi voluptatum possimus
           praesentium error omnis natus.
         </p>
@@ -23,7 +24,7 @@ const LatestCollecton = () => {
 
       {/* Rendering Products */}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
-        {latestProducts.map((product, index) => (
+        {bestSeller.map((product, index) => (
           <ProductItem key={index} id={product._id} image={product.image} name={product.name} price={product.price} />
         ))}
       </div>
@@ -31,4 +32,4 @@ const LatestCollecton = () => {
   );
 };
 
-export default LatestCollecton;
+export default BestSeller;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,15 @@
+import BestSeller from '../components/BestSeller';
 import Hero from '../components/Hero';
 import LatestCollecton from '../components/latestCollecton';
+import OurPolicy from '../components/OurPolicy';
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollecton />
+      <BestSeller />
+      <OurPolicy />
     </div>
   );
 };
